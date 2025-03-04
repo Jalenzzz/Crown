e94bc9462d07925a2ea8c7323bdbc0481691ba6f
PathoByte <pixie.trixieq@gmail.com>
2025-03-04 19:12:31 +0300

    first commit

---
+++ Venom_Code/snake.py
@@ -0,0 +1,8 @@
+# Variables hold data
+age = 25          # int (integer)
+height = 5.75      # float (decimal)
+name = "Charlie"   # str (string)
+is_student = True  # bool (boolean)
+
+print("height;",height)
+print("name;",name)
