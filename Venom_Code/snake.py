19695958366b8a17e2007ee593925fdb29535669
Jalenzz <126164913+Jalenzzz@users.noreply.github.com>
2025-03-11 15:46:47 +0300

    modified snake.py

--- Venom_Code/snake.py
+++ Venom_Code/snake.py
@@ -2,7 +2,7 @@
 age = 25          # int (integer)
 height = 5.75      # float (decimal)
 name = "Charlie"   # str (string)
-is_student = True  # bool (boolean)
+is_student = True
 
 print("height;",height)
 print("name;",name)
